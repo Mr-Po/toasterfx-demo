650222aa6b509e07f39761dcf032f0d32f8ddfe2
Mr.Po <ldd_live@foxmail.com>
2020-10-05 21:29:04 +0800

    完善测试

--- src/test/java/org/pomo/toasterfx/demo/test/DemoAppTest.java
+++ src/test/java/org/pomo/toasterfx/demo/test/DemoAppTest.java
@@ -15,11 +15,21 @@
  */
 package org.pomo.toasterfx.demo.test;
 
+import com.sun.media.jfxmedia.MediaException;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Throwables;
+import org.junit.Assert;
 import org.junit.Test;
 import org.pomo.toasterfx.demo.DemoApplication;
 import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+
+import java.util.Locale;
+import java.util.Set;
 
 /**
  * <h2>样例程序测试</h2>
@@ -40,6 +50,9 @@ public class DemoAppTest extends ApplicationTest {
 
     @Override
     public void init() {
+        WaitForAsyncUtils.autoCheckException = false;
+        WaitForAsyncUtils.printException = false;
+
         this.application = new DemoApplication();
     }
 
@@ -53,13 +66,81 @@ public class DemoAppTest extends ApplicationTest {
         this.application.stop();
     }
 
+    /**
+     * <h2>点击全部按钮</h2>
+     */
     @Test
-    public void testSuccessButton() {
-        clickOn("#btnSuccess");
+    public void execute() {
+
+        Set<Node> nodes = lookup(".button").queryAll();
+        nodes.forEach(this::click);
+
+        this.click(lookup("#radioThemeDark").query());
+        this.click(lookup("#radioRightTop").query());
+
+        ChoiceBox<Locale> cbxLanguage = lookup("#cbxLanguage").query();
+
+        this.click(cbxLanguage);
+        key(KeyCode.ENTER);
+        this.handleException(cbxLanguage);
+
+        this.click(cbxLanguage);
+        key(KeyCode.DOWN);
+        key(KeyCode.ENTER);
+        this.handleException(cbxLanguage);
+
+        this.click(cbxLanguage);
+        key(KeyCode.DOWN);
+        key(KeyCode.ENTER);
+        this.handleException(cbxLanguage);
+
+        nodes.forEach(this::click);
     }
 
-    @Test
-    public void testFailButton() {
-        clickOn("#btnFail");
+    /**
+     * <h2>点击</h2>
+     *
+     * @param node 节点
+     */
+    private void click(Node node) {
+
+        this.clickOn(node);
+        this.handleException(node);
+    }
+
+    /**
+     * <h2>处理异常</h2>
+     *
+     * @param node 节点
+     */
+    private void handleException(Node node) {
+
+        try {
+
+            WaitForAsyncUtils.checkException();
+
+        } catch (Throwable throwable) {
+
+            Throwable rootCause = Throwables.getRootCause(throwable);
+
+            // 排除音频无法播放的
+            if (!rootCause.getClass().isAssignableFrom(MediaException.class)
+                    && !"Could not create player!".equals(rootCause.getMessage())) {
+
+                log.error("handle: " + node, rootCause);
+
+                Assert.fail(Throwables.getRootCause(throwable).getMessage());
+            }
+        }
+    }
+
+    /**
+     * <h2>模拟键盘点击</h2>
+     *
+     * @param code 键位
+     */
+    private void key(KeyCode code) {
+        press(code);
+        release(code);
     }
 }
